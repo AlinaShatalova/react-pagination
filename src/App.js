24d17648347781ebdef7a7b5e477f4c4f5c0eff0
AlinaShatalova <lina.shatalova@yandex.ru>
2021-11-12 14:07:22 +0300

    load data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [loading, setLoading] = useState(false); 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [gamesPerPage] = useState(50);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      setLoading(true);
+      const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=100');
+      await loadingPokemon(res.data.results);
+      setLoading(false);
+    }
+
+    getPokemons();
+  }, [])
+
+  const loadingPokemon = async data => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await axios.get(pokemon.url);
+      return pokemonRecord.data;
+    }))
+
+    setPokemonData(_pokemonData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary">Pokemons</h1>
     </div>
   );
 }
