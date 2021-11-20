89f816cc5528c161ded290b6f7a6fe9e6e990d2d
AlinaShatalova <lina.shatalova@yandex.ru>
2021-11-20 14:10:40 +0300

    fix url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PokemonsList from "./components/PokemonsList";
+import PokemonsList from "./PokemonsList";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/react-pagination?" element={<PokemonsList />} />
+        <Route exact path="/react-pagination" element={<PokemonsList />} />
       </Routes>
     </Router>
   );
